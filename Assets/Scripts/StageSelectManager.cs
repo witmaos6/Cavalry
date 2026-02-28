c1fb042cdbad76962e702f72177dda288ebdd81a
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2026-03-01 00:59:17 +0900

    feat Home Stage UI logic
    스테이지 선택 시스템 로직 추가

---
+++ Assets/Scripts/StageSelectManager.cs
@@ -0,0 +1,53 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageSelectManager : MonoBehaviour
+{
+    public SaveData saveDataProcessor;
+    public GameObject stageButtonPrefab;
+    public Transform contentTransform;
+
+    private int maxTotalStage = 20;
+
+    private void OnEnable()
+    {
+        GenerateStageButtons();
+    }
+
+    void GenerateStageButtons()
+    {
+        foreach(Transform child in contentTransform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        GameData data = saveDataProcessor.LoadGame();
+        int availableStages = Mathf.Min(data.ClearStage + 1, maxTotalStage);
+
+        for(int i = availableStages; i >= 1; i--)
+        {
+            GameObject stageButton = Instantiate(stageButtonPrefab, contentTransform);
+
+            TMP_Text buttonText = stageButton.GetComponentInChildren<TMP_Text>();
+            if(buttonText != null )
+            {
+                buttonText.text = $"STAGE {i}";
+            }
+
+            int stageIndex = i;
+
+            Button buttonComp = stageButton.GetComponent<Button>();
+            if( buttonComp != null )
+            {
+                buttonComp.onClick.AddListener(() => OnStageButtonClicked(stageIndex));
+            }
+        }
+    }
+
+    void OnStageButtonClicked(int stageNum)
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
